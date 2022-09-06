1df1c41ae30e39df64be9a895f2a3dd52ef9703a
Champion-AS <108135994+Champion-AS@users.noreply.github.com>
2022-09-06 15:31:57 +0300

    1 + 7

--- js/task-01.js
+++ js/task-01.js
@@ -1,5 +1,33 @@
 // Напиши скрипт который:
 
 // Посчитает и выведет в консоль количество категорий в ul#categories, то есть элементов li.item.
-// Для каждого элемента li.item в списке ul#categories, найдет и выведет в консоль текст заголовка элемента (тега <h2>) и количество элементов в категории (всех вложенных в него <li>).
+
+// Для каждого элемента li.item в списке ul#categories, найдет и выведет в консоль текст заголовка
+// элемента(тега < h2 >) и количество элементов в категории(всех вложенных в него < li >).
+
 // В результате, в консоли будут выведены такие сообщения.
+
+// const elementList = document.querySelector('#categories').children;
+
+// console.log('Number of categories:', elementList.length);
+
+// [...elementList].forEach((item) => {
+//     const title = item.firstElementChild.textContent;
+//     const innerList = item.querySelectorAll('li').length;
+
+//     console.log('Category:', title);
+//     console.log('Elements:', innerList);
+
+// });
+
+// 2 варіант
+const elementList = document.querySelector('#categories');
+console.log('Number of categories:', elementList.children.length);
+
+[...elementList.children].forEach((item) => {
+    // console.log('Category:', item);
+    
+    console.log('Category:', item.querySelector('h2').textContent);
+    console.log('Elements:', item.querySelectorAll('li').length);
+
+});
--- js/task-07.js
+++ js/task-07.js
@@ -1,1 +1,12 @@
-// Напиши скрипт, который реагирует на изменение значения input#font-size-control (событие input) и изменяет инлайн-стиль span#text обновляя свойство font-size. В результате при перетаскивании ползунка будет меняться размер текста.
+// Напиши скрипт, который реагирует на изменение значения input#font-size-control
+// (событие input) и изменяет инлайн - стиль span#text обновляя свойство font - size.
+// В результате при перетаскивании ползунка будет меняться размер текста.
+
+const sizeContr = document.querySelector('#font-size-control');
+
+const text = document.querySelector('#text');
+
+sizeContr.addEventListener('input', (event) => {
+    text.style.fontSize =
+        `${event.currentTarget.value}px`; 
+});
